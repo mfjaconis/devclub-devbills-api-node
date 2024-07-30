5d5ad53ba901c98c697d21170efa5235f3f80d5e
mfjaconis <matheusjaconis@gmail.com>
2024-07-30 17:20:02 -0300

    criação das transações pt1

---
+++ src/services/transactions.service.ts
@@ -0,0 +1,40 @@
+import { StatusCodes } from "http-status-codes";
+import type { TransactionsRepository } from "../database/repositories/transactions.repository";
+import type { CreateTransactionDTO } from "../dtos/transactions.dto";
+import { Transaction } from "../entities/transactions.entity";
+import { AppError } from "../errors/app.error";
+import type { CategoriesRepository } from "./../database/repositories/categories.repository";
+
+export class TransactionsService {
+	constructor(
+		private transactionsRepository: TransactionsRepository,
+		private categoriesRepository: CategoriesRepository,
+	) {}
+
+	async create({
+		title,
+		date,
+		amount,
+		type,
+		categoryId,
+	}: CreateTransactionDTO): Promise<Transaction> {
+		const category = await this.categoriesRepository.findById(categoryId);
+
+		if (!category) {
+			throw new AppError("Category does not exists.", StatusCodes.NOT_FOUND);
+		}
+
+		const transaction = new Transaction({
+			title,
+			type,
+			date,
+			category,
+			amount,
+		});
+
+		const createdTransaction =
+			await this.transactionsRepository.create(transaction);
+
+		return createdTransaction;
+	}
+}
