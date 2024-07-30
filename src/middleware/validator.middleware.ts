c82b9dfc2b914ede3ccbbad492c83379fd2e5068
mfjaconis <matheusjaconis@gmail.com>
2024-07-30 15:53:31 -0300

    validação de dados com zod

---
+++ src/middleware/validator.middleware.ts
@@ -0,0 +1,34 @@
+// biome-ignore lint/style/useImportType: <explanation>
+import { NextFunction, Request, Response } from "express";
+// biome-ignore lint/style/useImportType: <explanation>
+import { z, ZodRawShape } from "zod";
+import { AppError } from "../errors/app.error";
+import { StatusCodes } from "http-status-codes";
+
+export enum ParamsType {
+	QUERY = "query",
+	BODY = "body",
+}
+
+type ValidateParams = {
+	schema: ZodRawShape;
+	type: ParamsType;
+};
+
+export function validator(params: ValidateParams) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const result = z.object(params.schema).safeParse(req[params.type]);
+
+		if (!result.success) {
+			const errorFormatted = result.error.issues.map(
+				(item) => `${item.path.join(".")}: ${item.message}`,
+			);
+
+			throw new AppError(errorFormatted, StatusCodes.UNPROCESSABLE_ENTITY);
+		}
+
+		req[params.type] = result.data;
+
+		next();
+	};
+}
--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
+
 import { CategoriesController } from "../controllers/categories.controllers";
+import { ParamsType, validator } from "../middleware/validator.middleware";
+import { createCategorySchema } from "../dtos/categories.dto";
 
 export const categoriesRoutes = Router();
 
 const controller = new CategoriesController();
 
-categoriesRoutes.post("/", controller.create);
+categoriesRoutes.post(
+	"/",
+	validator({
+		schema: createCategorySchema,
+		type: ParamsType.BODY,
+	}),
+	controller.create,
+);
